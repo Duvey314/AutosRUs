556009da0cbd53983fcde29fb82857df3324d90d
Duvey <rudow.david@gmail.com>
2020-09-07 17:17:54 -0500

    indexing and sampling

--- R_Analysis/01_Demo/file_read_demo.R
+++ R_Analysis/01_Demo/file_read_demo.R
@@ -1,4 +1,18 @@
+# install the libraries
+library(jsonlite)
+
+# read in our data files
 # demo_table <- read.csv(file='demo.csv',check.names=F,stringsAsFactors = F)
-#library(jsonlite)
-#demo_table2 <- fromJSON("http://citibikenyc.com/stations/json")
-demo_table2 <- fromJSON(txt='demo.json')
+# demo_table2 <- fromJSON(txt='demo.json')
+
+# index specific data
+claires_miles <- demo_table[2,"Total_Miles"]
+car_types <- demo_table$Vehicle_Class
+
+# filter data
+filter_table <- demo_table2[demo_table2$price > 10000,]
+filter_table2 <- subset(demo_table2, price > 10000 & drive == "4wd" & "clean" %in% title_status) #filter by price and drivetrain
+
+# sample data
+demo_table[sample(1:nrow(demo_table), 3),]
+
