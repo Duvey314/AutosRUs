455a28bc7ba52d72b8f7c0412a3c33a88e715ca9
Duvey <rudow.david@gmail.com>
2020-09-10 11:09:28 -0500

    files added for challenge, multiple linear regression performed on mpg data

---
+++ Challenge/MechaCarChallenge.R
@@ -0,0 +1,18 @@
+# import libraries
+library(ggplot2)
+library(dplyr)
+library(tidyverse)
+
+# pull in our data
+mpg_data <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+suspension <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+# perform multiple linear regression on mpg data to predict mpg
+head(mpg_data) # inspect the data
+lm(mpg ~ AWD + `vehicle weight` + `ground clearance` ,data=mpg_data)
+summary(lm(mpg ~ AWD + `vehicle weight` + `ground clearance` ,data=mpg_data))
+
+# only the ground clearance seemed to effect mpg. Lets try with other variables
+lm(mpg ~ `spoiler angle` + `vehicle length` + AWD + `ground clearance` ,data=mpg_data)
+summary(lm(mpg ~ `spoiler angle` + `vehicle length` + AWD + `ground clearance` ,data=mpg_data))
+
